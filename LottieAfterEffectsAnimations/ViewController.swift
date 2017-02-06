0ac72336592a79c28f15df16ce3731c9037550f7
NicoleYarroch <nicole@livio.io>
2017-02-06 13:04:24 -0500

    First commit

---
+++ LottieAfterEffectsAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LottieAfterEffectsAnimations
+//
+//  Created by Nicole on 2/6/17.
+//  Copyright © 2017 nicole@livio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
